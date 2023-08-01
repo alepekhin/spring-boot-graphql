532aeafdb7640b1db04c594e68ea7cd9e127aff0
alepekhin <alepekhine@gmail.com>
2023-08-01 14:17:14 +0300

    custom error handler added

---
+++ src/main/java/com/example/demo/common/CustomErrorHandler.java
@@ -0,0 +1,25 @@
+package com.example.demo.common;
+
+import graphql.GraphQLError;
+import graphql.GraphqlErrorBuilder;
+import graphql.schema.DataFetchingEnvironment;
+import org.springframework.graphql.execution.DataFetcherExceptionResolverAdapter;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Alex
+ * @since 01.08.2023
+ */
+@Component
+public class CustomErrorHandler extends DataFetcherExceptionResolverAdapter {
+
+    @Override
+    protected GraphQLError resolveToSingleError(Throwable ex, DataFetchingEnvironment env) {
+        return GraphqlErrorBuilder.newError()
+                .message(ex.getMessage())
+                .path(env.getExecutionStepInfo().getPath())
+                .location(env.getField().getSourceLocation())
+                .build();
+    }
+
+}
